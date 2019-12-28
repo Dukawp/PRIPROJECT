9cb48ffe80e42f3876b24942c67e482731193b6e
ricardoaneves <ricardo.neves97@gmail.com>
2019-12-28 16:18:42 +0000

    Updated models and JSONs

--- API/models/groups.js
+++ API/models/groups.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 var groupsSchema = new mongoose.Schema({
 
     name: {type: String, required: true},
-    photo: {type:String, required: true},
     descripton: String
 
 },
