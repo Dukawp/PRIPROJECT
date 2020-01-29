618da9784a009365fadba3ea2ca5e1ab63ec20f4
ricardoaneves <ricardo.neves97@gmail.com>
2020-01-29 19:32:11 +0000

    No more DeprecatedWarnings on Mongo connection.

--- API/app.js
+++ API/app.js
@@ -5,6 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
 mongoose.connect('mongodb://127.0.0.1:27017/TrabalhoPratico', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=>console.log('Mongo running...'))
   .catch((erro)=>console.log('Mongo: conection error  ' +erro));
