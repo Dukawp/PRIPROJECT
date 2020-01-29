642dbb0f622d2591e97b96a9be2649f8404ff7f7
Duarte Freitas <duka_duarte@hotmail.com>
2020-01-29 15:13:49 +0000

    send message

--- Interface/interface/public/javascripts/eventform.js
+++ Interface/interface/public/javascripts/eventform.js
@@ -18,4 +18,12 @@ $(()=>{
      //})           
      
   })
+
+  $(document).on("click", ".open-AddId", function () {
+     var myId = $(this).data('id');
+     $(".modal-body #myId").val( myId );
+     // As pointed out in comments, 
+     // it is unnecessary to have to manually call the modal.
+     // $('#addBookDialog').modal('show');
+});
   
